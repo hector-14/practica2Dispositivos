0d61b1deadceef2628973c5d4e1ae09e8abd4004
hector-14 <63135688+hector-14@users.noreply.github.com>
2022-09-28 12:24:26 -0500

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.p27s2120222hmrm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
